d29ec884b4a0491023fdd7691b8c0358cdd8ae50
HardPlant <abc7988se@naver.com>
2017-10-04 15:02:57 +0900

    initial python

---
+++ servertest.py
@@ -0,0 +1,17 @@
+import unittest
+
+class Servertest(unittest.TestCase):
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+
+    def testRed(self):
+        self.assertEqual(False,True, msg='Absolutely Not.')
+        self.assertAlmostEqual(10,10.5,msg="This is not Equal.")
+
+    def testGreen(self):
+        self.assertNotEqual(True,False)
+
+if __name__ == '__main__':
+    unittest.main()
