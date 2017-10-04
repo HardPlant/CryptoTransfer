357c28957fd78d22681fd7035539ccf5ec3f0735
HardPlant <abc7988se@naver.com>
2017-10-04 21:14:05 +0900

    programm skeleton

--- encrypted_chat.py
+++ encrypted_chat.py
@@ -5,10 +5,29 @@ import client
 def get_input(text):
     return input(text)
 
+def wait_for_response():
+    pass
+
+def try_to_connect():
+    return True, host, port
+
 if __name__ == '__main__':
     server = server.EchoServer()
     server.start()
-    client = client.Client()
+    get_connected = False
+    host = ''
+    port = 50007
+    print("Waiting for connect...")
+    print("You can connect with someone or wait for response.")
+    print("Want to connect with someone?(Y/N)")
+    if(input == 'Y'):
+        get_connected, host, port = try_to_connect()
+
+    if get_connected:
+        pass
+    else:
+        host, port = server.get_connector()
+    client = client.Client(host= host, port= port)
 
     while True:
         print()
