f7abe612bec52ab0136e8cfa915b0c37abd9d894
HardPlant <abc7988se@naver.com>
2017-10-04 18:18:33 +0900

    server get threaded

--- server.py
+++ server.py
@@ -13,36 +13,45 @@ def response(request):
 
 class EchoServer(threading.Thread):
     def __init__(self, host='127.0.0.1', port=50007):
+        threading.Thread.__init__(self)
         self.host = host
         self.port = port
 
+
     def run(self):
-        pass
+        self.boot()
+        self.echo()
+        self.conn.close()
 
     def boot(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((self.host, self.port))
         s.listen(1)
-        conn, addr = s.accept()
-        print('Connected by', addr)
-
-        self.echo(conn,addr)
-        conn.close()
+        self.conn, self.addr = s.accept()
+        print('Connected by', self.addr)
 
     def stop(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.host, self.host))
-        s.send('')
-        data = s.recv(1024)
-        s.close()
-        return self.server.join()
+        stopper = threading.Thread(target=self.send_null())
+        stopper.daemon = True
+        stopper.start()
 
-    def echo(self,conn,addr):
+    def echo(self):
         running = True
+        conn = self.conn
+        addr = self.addr
+
         while running is True:
             data = conn.recv(1024)
             if not data: break
-            conn.send(data)
+            res = response(data)
+            conn.send(res)
+
+    def send_null(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.host, self.host))
+        s.send('')
+        s.close()
+
 
 
 
