fe0e3b0ac379769647cfe2e8dd9cc3e8364b25ca
HardPlant <abc7988se@naver.com>
2017-10-04 15:25:05 +0900

    defined as func

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 # Echo server program
 import socket
+from multiprocessing import Process
 import CustomCrypto.mode.ECB
 
 HOST = ''                 # Symbolic name meaning the local host
 PORT = 50007              # Arbitrary non-privileged port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-while 1:
-    data = conn.recv(1024)
-    if not data: break
-    conn.send(data)
-conn.close()
+
+running = True
+def boot():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen(1)
+    print('Connected by', addr)
+    conn, addr = s.accept()
+
+    echo(conn,addr)
+    conn.close()
+
+def echo(conn,addr):
+    while running is True:
+        data = conn.recv(1024)
+        if not data: break
+        conn.send(data)
