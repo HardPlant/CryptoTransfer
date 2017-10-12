0c7a002ec6cd3322c7f19e5959bf117ad51034eb
HardPlant <abc7988se@naver.com>
2017-10-12 09:12:24 +0900

    Mac Added

---
+++ CustomCrypto/LEA/MAC.py
@@ -0,0 +1,43 @@
+from CustomCrypto.LEA import LEA
+
+
+def getMAC(pt, key, PKCSPadding = True):
+    if pt is None:
+        raise AttributeError('Improprt pt')
+    lea = LEA(key)
+
+    buffer = LEA.to_bytearray(pt)
+    chain_vec = buffer[0:16]
+    offset = 0
+    mac = bytearray()
+
+    len_x16 = len(buffer)-16
+    while offset <= len_x16:
+        chain_vec = lea.encrypt(LEA.xorAr(chain_vec, buffer[offset:offset+16]))
+
+        offset+= 16
+
+    if PKCSPadding:
+        more = len(buffer) - offset
+        buffer += bytearray([more])*more
+
+    LEA.xorAr(chain_vec, buffer)
+
+    return chain_vec
+
+
+
+
+
+
+def is_vailid(ct, mac):
+    pass
+
+
+
+
+
+
+
+
+
--- cryptotest.py
+++ cryptotest.py
@@ -1,16 +1,18 @@
 import unittest
-import struct
+
+from CustomCrypto.LEA import CBC
+from CustomCrypto.LEA.MAC import getMAC
+
 
 class testLea(unittest.TestCase):
-    def testbyte(self):
-        string = "aHello WorldaHello World"
-        ba = bytearray(string,'UTF-8')
-        for item in ba:
-            print(item)
-        print(ba[:8])
-        b = bytes(string,'UTF-8')
-        assert(ba[:8] == b[:8])
-        print(struct.unpack('<LLLL',ba[:16]))
+    def testMAC(self):
+        key = 'AAAAAAAAAAAAAAAA'
+        plaintext = 'Hello World, All of these days. who knows? let me show!'
+        tool = CBC(True, key, plaintext[0:8],PKCS5Padding=True)
+        crypt = tool.encrypt(plaintext)
+        crypt += tool.final()
+        print(crypt[:-16])
+        print(getMAC(plaintext, key))
 
 
 
